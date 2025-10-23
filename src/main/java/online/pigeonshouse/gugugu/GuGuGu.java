c7b44feb0c33bf2d7dbde88964b0ac9066b9a244
Admin <1942535787@qq.com>
2025-10-23 23:52:13 +0800

    0.2.1.3 fabric
    1. 为chatEvent components显示的面板添加点击复制组件名称的功能
    2. 更好的白名单，现在当启用whiteListDisableUidCheck且游戏并非在线时，玩家进入游戏会进入二次验证
        2.1 弹出验证条件：当玩家名称位于白名单，但是uuid无法对应上任意一个时，该玩家需要进行二次验证
            2.1.1 假设白名单有两个玩家[wmr(abcd-efgh)、wmr(aaaa-bbbb)]，此时玩家wmr(aaaa-bbbb)进入游戏，将不会要求进行二次验证，但若白名单中没有wmr(aaaa-bbbb)，则会要求wmr输入已在白名单中的wmr(abcd-efgh)的uuid来进行验证绑定
                2.1.1.1 绑定后会为wmr(aaaa-bbbb)同步wmr(abcd-efgh)的玩家数据
    
            2.1.2 当服务器有玩家 wmr(abcd-efgh)在线时，关联账户wmr(aaaa-bbbb)将会要求二次验证，验证成功则会将wmr(abcd-efgh)踢出服务器
        2.2 验证方式：玩家在聊天栏输入的第一句话即为验证信息，验证失败或长时间未输入会直接踢出服务器
    3. 统一玩家消息事件行为
    4. 重设计TickScheduler，增加循环任务
    5. 当Whitelist添加成功后会打印成功的玩家的uuid

--- src/main/java/online/pigeonshouse/gugugu/GuGuGu.java
+++ src/main/java/online/pigeonshouse/gugugu/GuGuGu.java
@@ -15,9 +15,12 @@ import online.pigeonshouse.gugugu.commands.TPFCommand;
 import online.pigeonshouse.gugugu.config.ModConfig;
 import online.pigeonshouse.gugugu.event.MinecraftServerEvents;
 import online.pigeonshouse.gugugu.fakeplayer.FakePlayerManager;
+import online.pigeonshouse.gugugu.fakeplayer.RIFakeServerPlayerFactory;
 import online.pigeonshouse.gugugu.fakeplayer.commands.RIFakePlayerCommands;
 import online.pigeonshouse.gugugu.fakeplayer.config.FakePlayerConfig;
 import online.pigeonshouse.gugugu.utils.TickScheduler;
+import online.pigeonshouse.gugugu.whitelist.WhitelistManage;
+import online.pigeonshouse.gugugu.whitelist.config.WhitelistConfig;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -30,6 +33,8 @@ public class GuGuGu implements ModInitializer {
     @Getter
     private FakePlayerConfig fakePlayerConfig;
     @Getter
+    private WhitelistConfig whitelistConfig;
+    @Getter
     private ModConfig config;
     @Getter
     private ChatEventHandler chatEventHandler;
@@ -37,10 +42,8 @@ public class GuGuGu implements ModInitializer {
     private FakePlayerManager fakePlayerManager;
     @Getter
     private BackupManager backupManager;
-
-    public GuGuGu() {
-        onInitialize();
-    }
+    @Getter
+    public MinecraftServer server;
 
     public void onInitialize() {
         INSTANCE = this;
@@ -52,14 +55,19 @@ public class GuGuGu implements ModInitializer {
         }
 
         this.config = new ModConfig(configDir.resolve("config.json").toFile());
+        this.whitelistConfig = new WhitelistConfig(configDir.resolve("whitelist.json").toFile());
         this.fakePlayerConfig = new FakePlayerConfig(configDir.resolve("fakeplayer_config.json").toFile());
 
         config.load();
         fakePlayerConfig.load();
+        whitelistConfig.load();
 
         chatEventHandler = new ChatEventHandler();
         fakePlayerManager = new FakePlayerManager(fakePlayerConfig);
         backupManager = new BackupManager();
+
+        WhitelistManage.init();
+        RIFakeServerPlayerFactory.init();
 
         MinecraftServerEvents.SERVER_TICK.addCallback(TickScheduler::onServerTick);
         MinecraftServerEvents.COMMAND_REGISTER.addCallback(this::registerCommands);
@@ -89,12 +97,13 @@ public class GuGuGu implements ModInitializer {
     }
 
     private void setup(MinecraftServerEvents.ServerStartedEvent event) {
-        MinecraftServer server = event.getServer();
+        server = event.getServer();
         fakePlayerManager.loginPersisted(server);
         log.info("[GuGuGu] Setup!");
     }
 
     private void stopped(MinecraftServerEvents.ServerStoppedEvent event) {
+        server = null;
         fakePlayerManager.recordAndSave(event.getServer());
         config.save();
         fakePlayerConfig.save();
--- src/main/java/online/pigeonshouse/gugugu/mixin/PlayerListMixin.java
+++ src/main/java/online/pigeonshouse/gugugu/mixin/PlayerListMixin.java
@@ -7,14 +7,21 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
 import online.pigeonshouse.gugugu.GuGuGu;
+import online.pigeonshouse.gugugu.event.MinecraftServerEvents;
 import online.pigeonshouse.gugugu.fakeplayer.RIFakeServerPlayer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerList.class)
 @Slf4j
 public class PlayerListMixin {
+    @Shadow @Final private MinecraftServer server;
+
     @Redirect(method = "placeNewPlayer",
             at = @At(value = "NEW",
                     target = "(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/network/Connection;Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/server/network/ServerGamePacketListenerImpl;"))
@@ -25,5 +32,12 @@ public class PlayerListMixin {
             return impl;
         }
         return new ServerGamePacketListenerImpl(server, connection, player);
+    }
+
+    @Inject(method = "placeNewPlayer",
+            at = @At(value = "RETURN"))
+    private void onPlayerJoinEvent(Connection connection, ServerPlayer player, CallbackInfo ci) {
+        MinecraftServerEvents.PlayerJoinEvent joinEvent = new MinecraftServerEvents.PlayerJoinEvent(server, player);
+        MinecraftServerEvents.PLAYER_JOIN.dispatch(joinEvent);
     }
 }
